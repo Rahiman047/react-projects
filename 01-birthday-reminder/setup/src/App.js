947bb02db9a69f3f345ced2ac478573fff63d527
Rahiman <abdurrahiman100@gmail.com>
2022-11-29 17:21:56 +0530

    birthday

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,34 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [objVal, changeObj] = useState({
+    count: `${data.length}`,
+    objList: data,
+  });
+
+  const clearAllVal = () => {
+    changeObj({ ...objVal, objList: [], count: 0 });
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{objVal.count} birthdays today</h3>
+        {objVal.objList.map((eachItem) => (
+          <List eachItem={eachItem} key={eachItem.id} />
+        ))}
+        <div>
+          <button
+            className="container button"
+            type="button"
+            onClick={clearAllVal}
+          >
+            Clear All
+          </button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = (listVal) => {
+  const { eachItem } = listVal;
+  console.log(eachItem);
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <article className="person">
+      <img src={eachItem.image} alt="person" />
+      <h4>{eachItem.name}</h4>
+      <p>{eachItem.age} years</p>
+    </article>
   );
 };
 

ff8884f2107c2b3a80e5f8ed20fb0558aaf99c11
Rahiman <abdurrahiman100@gmail.com>
2022-12-16 10:42:42 +0530

    lorem

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,51 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [number, setNumber] = useState();
+  const [para, setParagraphs] = useState([]);
+
+  const handleChange = (e) => {
+    const num = e.target.value;
+    setNumber(num);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Number(number) < 0) {
+      const val = data.slice(0, 1);
+      setParagraphs(val);
+    } else if (Number(number) > 8) {
+      const val = data.slice(0, 7);
+      setParagraphs(val);
+    } else {
+      const val = data.slice(0, number);
+      setParagraphs(val);
+    }
+  };
+
+  const renderSection = () => {
+    return (
+      <div>
+        {para.map((eachParagraph, index) => (
+          <p className="result" key={index}>
+            {eachParagraph}
+          </p>
+        ))}
+      </div>
+    );
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <main>
+      <h2>Tired of Boring Lorem Ipsum</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="para">Paragraphs :</label>
+        <input id="para" type="number" value={number} onChange={handleChange} />
+        <button className="btn">Generate</button>
+      </form>
+      <section className="lorem-form">{renderSection()}</section>
+    </main>
+  );
 }
 
 export default App;
